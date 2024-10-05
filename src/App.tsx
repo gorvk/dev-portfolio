295c764a811e66c5464d3f6e283765b516ec4fcb
Gourav kolhatkar <gstar1525@gmail.com>
2024-10-05 20:42:13 +0530

    decreased text size of contact links

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
 
   return (
     <div className="p-2">
-      <div className="text-2xl sticky top-2 rounded border-2 shadow-[4px_4px_0px_0px_black] border-black font-bold bg-white p-3 flex justify-between items-center">
-        <span>GK</span>
+      <div className="sticky top-2 rounded border-2 shadow-[4px_4px_0px_0px_black] border-black font-bold bg-white p-3 flex justify-between items-center">
+        <span className="text-xl">GK</span>
         <ContactBanner />
       </div>
       <div className="m-auto my-4 grid xl:grid-cols-4 md:grid-cols-2 gap-10 justify-evenly">
