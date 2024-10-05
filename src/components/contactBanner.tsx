90c9b06080132ccdce7ebafadd3e2788afe54b74
Gourav kolhatkar <gstar1525@gmail.com>
2024-10-05 20:39:00 +0530

    added resume link

--- src/components/contactBanner.tsx
+++ src/components/contactBanner.tsx
@@ -4,6 +4,7 @@ export const ContactBanner = () => {
     { label: "github", url: "https://github.com/gorvk" },
     { label: "linkedin", url: "https://linkedin.com/in/gorvk" },
     { label: "mail", url: "mailto:gstar1525@gmail.com" },
+    { label: "resume", url: "https://bit.ly/gorvkfe" },
   ];
   return (
     <span>
